9d7143721102ab368a981191b67631dba2959966
Lucas Romanó <luc4sromano@gmail.com>
2016-11-05 12:53:32 -0400

    Calculates biggest Norm and print screen. Add fadein of ExibirGrafico button
    Signed-off-by: Lucas Romanó <luc4sromano@gmail.com>

--- app/src/main/java/com/cw14/dbz_punching_machine/PunchActivity.java
+++ app/src/main/java/com/cw14/dbz_punching_machine/PunchActivity.java
@@ -1,13 +1,25 @@
 package com.cw14.dbz_punching_machine;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by lucas on 11/5/16.
@@ -15,14 +27,16 @@ import android.widget.TextView;
 
 public class PunchActivity extends AppCompatActivity  {
 
-    private SensorEventListener myAccelerometerListener, myMagneticFieldListener;
+    private SensorEventListener myAccelerometerListener;
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
-    //private Sensor magField;
     private Boolean mInitialized = false;
     private float mLastX, mLastY, mLastZ;
-    private float[] accelerometerValues, magneticFieldValues;
-    private float[] outR = new float[9];
+
+    Button showGraphFadeIn;
+
+    List<Double> results = new ArrayList<Double>();
+    Double resultado;
 
     // Get norm given a vector.
     protected double getNorm(float x, float y, float z) {
@@ -41,10 +55,12 @@ public class PunchActivity extends AppCompatActivity  {
         mSensorManager.registerListener(myAccelerometerListener, mAccelerometer,
                 SensorManager.SENSOR_DELAY_NORMAL);
 
+        showGraphFadeIn = (Button) findViewById(R.id.showGraphFadeBt);
+
         myAccelerometerListener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent event) {
-                float NOISE = 0;
+                float NOISE = 0.2f;
                 TextView tvX = (TextView) findViewById(R.id.x_axis);
                 TextView tvY = (TextView) findViewById(R.id.y_axis);
                 TextView tvZ = (TextView) findViewById(R.id.z_axis);
@@ -73,6 +89,7 @@ public class PunchActivity extends AppCompatActivity  {
                     tvX.setText(Float.toString(deltaX));
                     tvY.setText(Float.toString(deltaY));
                     tvZ.setText(Float.toString(deltaZ));
+                    results.add(getNorm(deltaX, deltaY, deltaZ));
                 }
             }
 
@@ -81,6 +98,35 @@ public class PunchActivity extends AppCompatActivity  {
 
             }
         };
+
+        new CountDownTimer(4000, 1000) {
+            TextView countdown = (TextView) findViewById(R.id.countdown);
+            public void onTick(long millisUntilFinished) {
+                countdown.setText("" + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                countdown.setText("Fim!");
+                resultado = Collections.max(results);
+                countdown.setText("" + resultado);
+                mSensorManager.unregisterListener(myAccelerometerListener);
+
+                //show grafico fade in
+                Animation fadeIn = new AlphaAnimation(0, 1);
+                fadeIn.setInterpolator(new AccelerateInterpolator()); //add this
+                fadeIn.setDuration(1000);
+                fadeIn.start();
+
+                showGraphFadeIn = (Button) findViewById(R.id.showGraphFadeBt);
+
+                AnimationSet animation = new AnimationSet(false);
+                animation.addAnimation(fadeIn);
+                showGraphFadeIn.setAnimation(animation);
+                showGraphFadeIn.setAlpha(1);
+            }
+        }.start();
+
+
     }
 
     protected void onResume() {
